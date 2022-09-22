833296629609764ca5531faa5c0d7d157671f162
Alejandro Mejia Jaramillo <almejiaja@unal.edu.co>
2022-09-22 00:30:59 -0500

    Mejoras código sistema de navegación y prueba 23 septiembre

--- CONTROL_SYSTEM/Bluethoot_mega/Bluethoot_mega.ino
+++ CONTROL_SYSTEM/Bluethoot_mega/Bluethoot_mega.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
 SoftwareSerial BT(10, 11);   // Definimos los pines RX y TX del Arduino conectados al Bluetooth
+
 #include <Servo.h>
 
-Servo esc1;
-Servo esc2;
+Servo esc1, esc2;
 
 char valor;
 String estado;
@@ -14,6 +14,15 @@ float b = 1;
 int min_esc = 1000;
 int max_esc = 2000;
 
+const byte numChars = 255;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+int dataNumber = 0;             // new for this version
+
+
+
 void setup()
 {
   esc1.attach(5, min_esc, max_esc);// motor izquierdo m1 al pin 5
@@ -23,52 +32,15 @@ void setup()
   esc1.writeMicroseconds(0);
   esc2.writeMicroseconds(0);
   Serial.begin(9600);   // Inicializamos  el puerto serie
+  Serial1.begin(9600);   // Inicializamos  el puerto serie
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  if (BT.available())   // Si llega un dato por el puerto BT se envía al monitor serial
-  {
-    valor = BT.read();
-
-    Serial.println(valor);
-    switch (valor) {
-      case 'A': //Mover los dos motores al tiempo a una potencia asignada desde app
-        m12();
-        break;
-      case 'B': //
-        m2();
-        break;
-      case 'a':
-        move_usv(a * b, a);
-        Serial.println("Adelante");
-        Serial.println(a);
-        Serial.println(a * b);
-        break;
-      case 'b':
-        move_usv(0, 1800);
-        Serial.println("Izquierda"); //prende motor derecho pin 9
-        Serial.println(a);
-        Serial.println(a * b);
-        break;
-      case 'c':
-        move_usv(1800 * b, 0);
-        Serial.println("Derecha"); //prende motor izquierdo pin 9
-        Serial.println(1800);
-        Serial.println(1800 * b);
-        break;
-      case 'd':
-        parar();
-        break;
-      case 'P':
-        zigzag(1000, 2);
-        break;
-    }
-
- 
-  }
-
+  dataBT();
 }
+
 
 void m1() {
   delay (30);
@@ -84,6 +56,53 @@ void m1() {
 
 }
 
+void dataBT () {
+  if (BT.available()) { // Si llega un dato por el puerto BT se envía al monitor serial
+    valor = BT.read();
+
+    switch (valor) {
+      case 'A': //Mover los dos motores al tiempo a una potencia asignada desde app
+        m12();
+        break;
+      case 'a':
+        move_usv(a * b, a);
+        //        Serial.println("Adelante");
+        //        Serial.println(a);
+        //        Serial.println(a * b);
+        break;
+      case 'b':
+        move_usv(0, a);
+        //        Serial.println("Izquierda"); //prende motor derecho pin 9
+        //        Serial.println(a);
+        //        Serial.println(a * b);
+        break;
+      case 'c':
+        move_usv(a * b, 0);
+        //        Serial.println("Derecha"); //prende motor izquierdo pin 9
+        //        Serial.println(1500);
+        //        Serial.println(1500 * b);
+        break;
+      case 'd':
+        parar();
+        break;
+
+      case 'P':
+        digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+        break;
+
+      case 'S':
+        recvWithStartEndMarkers();
+        showNewData();
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+
+        break;
+    }
+
+  }
+
+}
+
 void m12() { //mover motores al valor que llegue por bt
   delay (30);
   while (BT.available()) {
@@ -93,26 +112,12 @@ void m12() { //mover motores al valor que llegue por bt
   if (estado.length() > 0) {
     esc1.writeMicroseconds(estado.toInt() * b); //motor izquierdo (debe conectarse al 5)
     esc2.writeMicroseconds((estado.toInt())); //motor derecho (debe conectarse al 9)
-    Serial.println(estado.toInt());
-    Serial.println(estado.toInt() * b);
+    //    Serial.println(estado.toInt());
+    //    Serial.println(estado.toInt() * b);
     estado = "";
   }
-
 }
 
-void m2() {
-  delay (30);
-  while (BT.available()) {
-    char c = BT.read();
-    estado += c;
-  }
-  if (estado.length() > 0) {
-    esc2.writeMicroseconds(estado.toInt());
-    Serial.println(estado.toInt());
-    estado = "";
-  }
-
-}
 
 void parar() {
   esc1.writeMicroseconds(0);
@@ -120,23 +125,48 @@ void parar() {
   Serial.println("Parar");
 }
 
+
 void move_usv(int m1, int m2) {
   esc1.writeMicroseconds(m1);
   esc2.writeMicroseconds(m2);
 }
 
-void zigzag(int t, int count) {
 
-  for (int i = 0; i <= count; i++) {
-    move_usv(1600, 0);
-    delay(t);
-    move_usv(1600, 1600);
-    delay(t/2);
-    move_usv(0, 1600);
-    delay(t);
-    move_usv(1600, 1600);
-    delay(t/2);
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial1.available() > 0 && newData == false) {
+    rc = Serial1.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
   }
-  parar();
+}
 
+void showNewData() {
+  if (newData == true) {
+    Serial.println(receivedChars);
+    newData = false;
+  }
 }
--- NAVIGATION_SYSTEM/Kalman_GPS_IMU/Kalman_GPS_IMU/Kalman_GPS_IMU.ino
+++ NAVIGATION_SYSTEM/Kalman_GPS_IMU/Kalman_GPS_IMU/Kalman_GPS_IMU.ino
@@ -2,7 +2,6 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Kalman1.h>
-//#include <JY901_1.h>
 #include <SD.h>
 #include <SPI.h>
 #include <JY901.h>
@@ -10,8 +9,10 @@
 static const int RXPin = 4, TXPin = 3; //Comunicación GPS
 static const uint32_t GPSBaud = 9600; //Velocidad GPS
 
+
 // The TinyGPSPlus object
 TinyGPSPlus gps;
+
 
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
@@ -38,8 +39,8 @@ using namespace BLA;
 //#define m1 0.01
 //#define m2 0.02
 
-#define m1 0.1
-#define m2 0.2
+#define m1 0.01
+#define m2 0.02
 
 #define r_a  6378137.0 //IERS  WGS-84 ellipsoid, semi-major axis (a) 6378137.0
 #define r_b  6356752.3142 //IERS  WGS-84 ellipsoid, semi-minor axis (b) 6356752.3142
@@ -53,7 +54,7 @@ BLA::Matrix<Nobs> obs; // observation vector
 //------------------------------------
 
 float a_x, a_y, a_z, yaw, pitch, roll, a_abs, pos, vel, vel_0, pos_0;
-float bias_ax, bias_ay, bias_az;
+//float bias_ax, bias_ay, bias_az;
 
 //------------------------------------
 /****       GPS PARAMETERS    ****/
@@ -61,24 +62,20 @@ float bias_ax, bias_ay, bias_az;
 
 float latt, longi, alt, vel_gps;
 //Coordenada LLT inicial
-float la_o = 6.236097;
-float lo_o = -75.547851;
-float alt_o = 1680;
+
+float la_o = 6.236123;
+float lo_o = -75.547935;
+float alt_o = 1656;
 
 float ecef_a[3]; //coordenadas X, Y y Z de la posición actual
 float ecef_o[3]; //coordenadas X, Y y Z de la posición inicial
 float ned[3]; //Posición NED GPS
 
-float vn = 0;
-
-
 long timer = 0; //general purpuse timer
 long timer_old;
 float G_Dt; // delay between two updates of the filter
 
-// file name to use for writing
-const char filename[] = "demo.txt";
-File dataFile;
+
 
 void setup() {
 
@@ -86,8 +83,7 @@ void setup() {
   ss.begin(GPSBaud);
   JY901.attach(Serial);
 
-
-  //  for (int i = 0; i < 100; i++) // We take some readings...
+  //  for (int i = 0; i < 5000; i++) // We take some readings...
   //  {
   //    JY901.receiveSerialData();
   //    bias_ax += ((float)JY901.getAccX());
@@ -96,27 +92,11 @@ void setup() {
   //    delay(10);
   //  }
   //
-  //  bias_ax = bias_ax / 100;
-  //  bias_ay = bias_ay / 100;
-  //  bias_az = bias_az / 100;
+  //  bias_ax = bias_ax / 5000;
+  //  bias_ay = bias_ay / 5000;
+  //  bias_az = bias_az / 5000;
 
   //Serial.println(bias_ax, 3); Serial.println(bias_ay, 3); Serial.println(bias_az, 3);
-
-  pinMode(2, OUTPUT);
-
-  Serial.print("Initializing SD card...");
-  //
-  
-  // see if the card is present and can be initialized:
-  if (!SD.begin(10)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-
-  Serial.println("card initialized.");
-
-
 
   // example of evolution matrix. Size is <Nstate,Nstate>
   K.F = {1.0, 0.0,
@@ -139,114 +119,127 @@ void setup() {
          0.0, m2 * m2
         };
 
-  delay(5000);
+  delay(1000);
   timer = millis();
 
 }
 
 void loop() {
 
-  if ((millis() - timer) >= 10) // Main loop runs at 100Hz
+  timer_old = timer;
+  timer = millis();
+
+  if (timer > timer_old)
   {
+    G_Dt = (timer - timer_old) / 1000.0; // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
 
-    timer_old = timer;
-    timer = millis();
-    if (timer > timer_old)
-    {
-      G_Dt = (timer - timer_old) / 1000.0; // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
-      if (G_Dt > 0.1)
-        G_Dt = 0; // ignore integration times over 100 ms
-    }
-    else
-      G_Dt = 0;
-
-    JY901.receiveSerialData();
-    //    a_x = ((float)JY901.stcAcc.a[0] / 32768 * 16);
-    //    a_y = ((float)JY901.stcAcc.a[1] / 32768 * 16);
-    //    a_z = ((float)JY901.stcAcc.a[2] / 32768 * 16);
-    //    roll =  ((float)JY901.stcAngle.Angle[0] / 32768 * 180);
-    //    pitch = ((float)JY901.stcAngle.Angle[1] / 32768 * 180);
-    //    yaw = ((float)JY901.stcAngle.Angle[2] / 32768 * 180);
-
-    a_x = ((float)JY901.getAccX());
-    a_y = ((float)JY901.getAccY());
-    a_z = ((float)JY901.getAccZ());
-    roll=((float)JY901.getRoll());
-    pitch=((float)JY901.getPitch());
-    yaw=((float)JY901.getYaw());
-
-    a_abs = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
-
-    a_abs = abs(a_abs - 1.055697); //se resta valor de la gravedad
-
-    //a_abs = a_abs * 9.8;
-
-    vel = vel_0 + (a_abs * G_Dt);
-    pos = pos_0 + vel_0 * G_Dt + ((G_Dt * G_Dt) * 0.5 * a_abs); //Vector de posicion medido con el IMU
-
-    vel_0 = vel;
-    pos_0 = pos;
-
-    //    if (a_abs > 0.025) { //https://sci-hub.se/10.1109/tencon.2017.8227906
-    //      vel = vel + a_abs * 9.8 * G_Dt;
-    //      float pos_a = pos;
-    //      pos = pos + vel + a_abs * 9.8 * ((G_Dt * G_Dt) * 0.5);//Vector de posicion medido con el IMU
-    //    } else {
-    //      vel = 0;
-    //    }
-
-    while (ss.available() > 0) {
-      if (gps.encode(ss.read())) {
-
-        latt = validGPS(gps.location.lat(), gps.location.isValid());
-        longi = validGPS(gps.location.lng(), gps.location.isValid());
-        alt = validGPS(gps.altitude.meters(), gps.altitude.isValid());
-        vel_gps = validGPS(gps.speed.mps(), gps.speed.isValid());
-        llt2ecef(ecef_a, latt, longi, alt);
-        llt2ecef(ecef_o, la_o, lo_o, alt_o);
-        ecef2ned(la_o, lo_o, ecef_o[0], ecef_o[1], ecef_o[2], ecef_a[0], ecef_a[1], ecef_a[2]);
-        obs(0) = sqrt(ned[0] * ned[0] + ned[1] * ned[1]);
-        obs(1) = vel_gps;
-        //Serial.print(latt, 6); Serial.print(F(";")); Serial.print(longi, 6); Serial.print(F(";")); Serial.print(alt); Serial.print(F(";")); Serial.println(vel_gps);
-      }
-    }
-
-    K.F = {1.0,  0.0,
-           0.0,  G_Dt
-          };
-
-    K.B = {(G_Dt * G_Dt)*a_abs / 2,
-           G_Dt * a_abs
-          };
-
-
-    K.update(obs);
-
-    // PRINT RESULTS: measures and estimated state
-    //Serial << obs << ' ' << K.x << ' ' << G_Dt <<  ' ' << a_abs << ' ' << pos << ' ' << vel << '\n';
-
-    //Serial.println(a_abs, 6);
-
-    //Serial  << a_x << ' ' << a_y <<  ' ' << a_z << ' ' << a_abs << ' ' << pos << ' ' << vel << '\n';
-    Serial.print(a_x, 4); Serial.print(F(";")); Serial.print(a_y, 4); Serial.print(F(";")); Serial.print(a_z, 4); 
-    Serial.print(F(";")); Serial.print(a_abs, 6);Serial.print(F(";"));
-    Serial.print(roll, 4); Serial.print(F(";")); Serial.print(pitch, 4); Serial.print(F(";")); Serial.print(yaw, 4); 
-    
-    
-    
-    Serial.print(F(";")); Serial.println(G_Dt, 4);
-
-
+    if (G_Dt > 0.2)
+      G_Dt = 0; // ignore integration times over 100 ms
   }
-  //delay(1);
+  else {
+    G_Dt = 0;
+  }
+
+  JY901.receiveSerialData();
+  //    a_x = ((float)JY901.stcAcc.a[0] / 32768 * 16);
+  //    a_y = ((float)JY901.stcAcc.a[1] / 32768 * 16);
+  //    a_z = ((float)JY901.stcAcc.a[2] / 32768 * 16);
+  //    roll =  ((float)JY901.stcAngle.Angle[0] / 32768 * 180);
+  //    pitch = ((float)JY901.stcAngle.Angle[1] / 32768 * 180);
+  //    yaw = ((float)JY901.stcAngle.Angle[2] / 32768 * 180);
+
+  a_x = ((float)JY901.getAccX());
+  a_y = ((float)JY901.getAccY());
+  a_z = ((float)JY901.getAccZ());
+  roll = ((float)JY901.getRoll());
+  pitch = ((float)JY901.getPitch());
+  yaw = ((float)JY901.getYaw());
+
+  float a_x_p = ((8.238905 * pow(10, -7)) * (pitch * pitch * pitch)) + ((9.846454 * pow(10, -6)) * (pitch * pitch)) - (0.017688 * pitch) + 0.003106;
+  float a_x_d = (a_x - 0.003106 ) - a_x_p;
+  float a_x_dx = a_x_d * cos(ToRad(pitch));
+  float a_x_dy = a_x_d * sin(ToRad(pitch));
+
+
+  float a_y_p = ((-8.026296 * pow(10, -7)) * (roll * roll * roll)) - ((1.018808 * pow(10, -6)) * (roll * roll)) + (0.017629 * roll) + 0.000581;
+  float a_y_d =  (a_y - 0.000581) - a_y_p;
+  float a_y_dx = a_y_d * cos(ToRad(roll));
+  float a_y_dy = a_y_d * sin(ToRad(roll));
+
+  //    a_abs = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
+  //    a_abs = abs(a_abs - 1.055697); //se resta valor de la gravedad
+
+  a_abs = sqrt(a_x_d * a_x_d + a_y_d * a_y_d);
+  //a_abs = a_abs * 9.8;
+
+  vel = vel_0 + (a_abs * G_Dt);
+  pos = pos_0 + vel_0 * G_Dt + ((G_Dt * G_Dt) * 0.5 * a_abs); //Vector de posicion medido con el IMU
+
+  vel_0 = vel;
+  pos_0 = pos;
+
+  //    if (a_abs > 0.025) { //https://sci-hub.se/10.1109/tencon.2017.8227906
+  //      vel = vel + a_abs * 9.8 * G_Dt;
+  //      float pos_a = pos;
+  //      pos = pos + vel + a_abs * 9.8 * ((G_Dt * G_Dt) * 0.5);//Vector de posicion medido con el IMU
+  //    } else {
+  //      vel = 0;
+  //    }
+
+  K.F = {1.0,  0.0,
+         0.0,  G_Dt
+        };
+
+  K.B = {(G_Dt * G_Dt)*a_abs / 2,
+         G_Dt * a_abs
+        };
+
+
+  K.update(obs);
+
+  ss.listen();
+
+  while (ss.available() > 0) {
+
+    if (gps.encode(ss.read())) {
+
+      latt = validGPS(gps.location.lat(), gps.location.isValid());
+      longi = validGPS(gps.location.lng(), gps.location.isValid());
+      alt = validGPS(gps.altitude.meters(), gps.altitude.isValid());
+      vel_gps = validGPS(gps.speed.mps(), gps.speed.isValid());
+      llt2ecef(ecef_a, latt, longi, alt);
+      llt2ecef(ecef_o, la_o, lo_o, alt_o);
+      ecef2ned(la_o, lo_o, ecef_o[0], ecef_o[1], ecef_o[2], ecef_a[0], ecef_a[1], ecef_a[2]);
+      obs(0) = sqrt(ned[0] * ned[0] + ned[1] * ned[1]);
+      obs(1) = vel_gps;
+
+    }
+  }
+
+  Serial.print('<');
+
+  Serial.print(a_x, 3); Serial.print(F(",")); Serial.print(a_x_p, 3);  Serial.print(F(",")); Serial.print(a_x_d, 3); Serial.print(F(","));
+  Serial.print(pitch); Serial.print(F(","));
+
+  Serial.print(a_y, 3); Serial.print(F(",")); Serial.print(a_y_p, 3);  Serial.print(F(",")); Serial.print(a_y_d, 3); Serial.print(F(","));
+  Serial.print(roll); Serial.print(F(","));
+
+  Serial.print(a_abs); Serial.print(F(",")); Serial.print(pos);  Serial.print(F(",")); Serial.print(vel); Serial.print(F(","));
+  Serial.print(yaw); Serial.print(F(","));  Serial.print(G_Dt, 4); Serial.print(F(","));
+
+  Serial.print(latt, 6); Serial.print(F(",")); Serial.print(longi, 6);  Serial.print(F(",")); Serial.print(ned[0]); Serial.print(F(","));
+  Serial.print(ned[1]); Serial.print(F(",")); Serial.print(obs(0)); Serial.print(F(",")); Serial.print(obs(1));
+  Serial.print(F(",")); Serial.print(K.x(0)); Serial.print(F(",")); Serial.print(K.x(1));
+
+  Serial.println('>');
+
 
 }
-
 
 void llt2ecef(float * ecef, float lattitud, float longitud, float high) {
   lattitud = ToRad(lattitud);
   longitud = ToRad(longitud);
-  vn = r_a / sqrt(1 - e * (sin(lattitud) * sin(lattitud))); //Elipsoide
+  float vn = r_a / sqrt(1 - e * (sin(lattitud) * sin(lattitud))); //Elipsoide
   ecef[0] = (vn + high) * cos(lattitud) * cos(longitud);
   ecef[1] = (vn + high) * cos(lattitud) * sin(longitud);
   ecef[2] = (vn * (1 - (e * e)) + high) * sin(lattitud);
@@ -274,62 +267,4 @@ float validGPS(float val, bool valid)
   {
     return  0;
   }
-}
-
-//void serialEvent()
-//{
-//  while (Serial.available())
-//  {
-//    JY901.CopeSerialData(Serial.read()); //Call JY901 data cope function
-//
-//
-//        File dataFile = SD.open(filename, FILE_WRITE);
-//        //   if the file is available, write to it:
-//        if (dataFile) {
-//          digitalWrite(2 , HIGH);
-//          //dataFile.print(a_x); dataFile.print(","); dataFile.print(a_y); dataFile.print(","); dataFile.print(a_z); dataFile.print(","); dataFile.println(a_abs);
-//          dataFile.print(latt, 6); dataFile.print(","); dataFile.print(longi, 6); dataFile.print(","); dataFile.print(alt); dataFile.print(","); dataFile.println(vel_gps);
-//
-//          dataFile.close();
-//          // print to the serial port too:
-//          Serial.print(a_x); Serial.print(F(";")); Serial.print(a_y); Serial.print(F(";")); Serial.print(a_z); Serial.print(F(";")); Serial.println(a_abs);
-//          Serial.print(latt, 6); Serial.print(","); Serial.print(longi, 6); Serial.print(","); Serial.print(alt); Serial.print(","); Serial.println(vel_gps);
-//          digitalWrite(2 , LOW);
-//        }
-//        // if the file isn't open, pop up an error:
-//        else {
-//          digitalWrite(2 , LOW);
-//          Serial.println("error opening demo.txt");
-//        }
-//
-//  }
-//}
-
-void save_data() {
-
-  dataFile = SD.open(filename, FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    //dataFile.println(a_x);
-    //dataFile.write(a_x);
-    //dataFile.write(latt); dataFile.write(F(";")); dataFile.write(longi); dataFile.write(F(";")); dataFile.write(alt); dataFile.write(F(";")); dataFile.write(vel_gps);
-    digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-
-    dataFile.write(obs(0)); dataFile.write(F(";")); dataFile.write(obs(1)); dataFile.write("\r\n");
-    dataFile.close();
-    digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
-
-    // print to the serial port too:
-    //Serial.println(a_x);
-    Serial.print(obs(0)); Serial.print(F(";")); Serial.println(obs(1));
-
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
-
-    Serial.println("error opening datalog.txt");
-  }
-
 }
